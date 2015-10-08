52a9375e7862a4304d3ab2bac10b39e3e29b3d2e
Roberto Edgar Geiss <rgeiss@ig.com.br>
2015-10-08 16:39:59 -0300

    MongoDB em Heroku 2

--- config/connections.js
+++ config/connections.js
@@ -6,8 +6,8 @@ module.exports.connections =
 	    url: mongodb://heroku_mvnfwfr5:fv4kraui3f485nmuujn4667o31@ds035014.mongolab.com:35014/heroku_mvnfwfr5
 	    //host: 'localhost',
 	    //port: 27017,
-	    user: 'roberto',
-	    password: 'teste',
-	    database: 'test'
+	    //user: 'roberto',
+	    //password: 'teste',
+	    //database: 'test'
   	}
 };
