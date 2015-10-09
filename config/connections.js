8d4a908fc22200a8373a9eb263a3608cee365f53
Roberto Edgar Geiss <rgeiss@ig.com.br>
2015-10-08 21:05:39 -0300

    MongoDB em Heroku 3

--- config/connections.js
+++ config/connections.js
@@ -3,7 +3,7 @@ module.exports.connections =
  	Mongodb: 
 	{
 	    adapter: 'sails-mongo',
-	    url: mongodb://heroku_mvnfwfr5:fv4kraui3f485nmuujn4667o31@ds035014.mongolab.com:35014/heroku_mvnfwfr5
+	    url: 'mongodb://heroku_mvnfwfr5:fv4kraui3f485nmuujn4667o31@ds035014.mongolab.com:35014/heroku_mvnfwfr5'
 	    //host: 'localhost',
 	    //port: 27017,
 	    //user: 'roberto',
